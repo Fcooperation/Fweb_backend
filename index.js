8633e3fddf3310a784e9779bad3c30bcd0e229f7
Fcooperation <thefcooperation@gmail.com>
2025-07-05 11:14:08 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/search', (req, res) => {
+  const { query } = req.body;
+  console.log('Received search query:', query);
+  res.json({ message: 'Hello from backend', yourQuery: query });
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
+});
