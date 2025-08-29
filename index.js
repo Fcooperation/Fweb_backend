1e9aa0e27e713484d8536dfd81da8f208e3d7307
Fcooperation <thefcooperation@gmail.com>
2025-08-29 08:45:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json());
+
+app.post('/fcards', (req, res) => {
+  const query = req.body.query;
+  console.log("Received query:", query);
+  res.json({ fcards: [{ title: "Hi", link: "#", snippet: "Hello from backend!" }] });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
