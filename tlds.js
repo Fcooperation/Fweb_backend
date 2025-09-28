888f289ae02e413bec002a6d94296fd7c9eae4eb
Fcooperation <thefcooperation@gmail.com>
2025-09-28 19:28:41 +0100

    Create tlds.js

---
+++ tlds.js
@@ -0,0 +1,16 @@
+// tlds.js
+export const TLDs = [
+  // 🔹 Generic / global
+  ".com", ".org", ".net", ".io", ".co", ".ai", ".dev", ".app", ".info", ".edu",
+  ".biz", ".tech", ".online", ".site", ".store", ".web", ".me", ".cloud", ".xyz", ".pro",
+  ".global", ".today", ".news", ".media", ".agency", ".solutions", ".systems", ".services",
+  ".group", ".network", ".digital", ".world", ".center", ".space", ".company", ".studio", ".website",
+  ".live", ".press", ".tv", ".fm", ".music", ".video", ".photo", ".pictures", ".design",
+
+  // 🔹 African country TLDs
+  ".ng", ".za", ".ke", ".gh", ".tz", ".ug", ".eg", ".dz", ".ma", ".sn",
+  ".ci", ".ml", ".cm", ".et", ".mu", ".bw", ".na", ".rw", ".zw", ".zm",
+  ".ls", ".mg", ".sd", ".tn", ".ly", ".gn", ".bf", ".cg", ".gq", ".er",
+  ".dj", ".sc", ".km", ".so", ".st", ".cv", ".mw", ".ao", ".ne", ".td",
+  ".mr", ".ss", ".ga", ".bi", ".bj", ".sz", ".tg", ".gm", ".lr", ".sl"
+];
