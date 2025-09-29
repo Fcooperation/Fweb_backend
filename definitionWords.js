bcd2bcbac806393d8a144d8d9f75e8a1dd1a143d
Fcooperation <thefcooperation@gmail.com>
2025-09-29 11:21:14 +0100

    Create definitionWords.js

---
+++ definitionWords.js
@@ -0,0 +1,16 @@
+// definitionWords.js
+export const definitionWords = [
+  "define",
+  "definition of",
+  "what is",
+  "what's",
+  "how to",
+  "explain",
+  "meaning of",
+  "describe",
+  "tell me about",
+  "give me info on",
+  "information about",
+  "what are",
+  "who is"
+];
