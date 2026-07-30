9a1f50186abd39dc00ffc3d539469a4af613add6
Fcooperation <thefcooperation@gmail.com>
2026-07-30 13:43:14 +0100

    Add files via upload

--- send-message.js
+++ send-message.js
@@ -1,15 +1,109 @@
-export default
-async function sendMessage(
+import {
+createClient
+}
+from "@supabase/supabase-js";
+
+import "dotenv/config";
+
+const supabaseUrl =
+process.env.SUPABASE_URL;
+
+const supabaseKey =
+process.env.SUPABASE_KEY;
+
+const supabase =
+createClient(
+supabaseUrl,
+supabaseKey
+);
+
+export default async function sendMessage(
 data
 ){
 
-return {
+try{
 
-success: true,
+const {
+
+messageId,
+
+senderId,
+
+receiverId,
+
+message,
+
+replyToId
+
+} = data;
+
+const {
+error
+} =
+await supabase
+.from("messages")
+.insert({
+
+message_id:
+messageId,
+
+sender_id:
+senderId,
+
+receiver_id:
+receiverId,
+
+message,
+
+reply_to_id:
+replyToId,
+
+status:
+"sent"
+
+});
+
+if(error){
+
+console.error(
+error
+);
+
+return{
+
+success:false,
 
 message:
-"Message received"
+error.message
 
 };
+
+}
+
+return{
+
+success:true,
+
+message:
+"Message sent"
+
+};
+
+}catch(err){
+
+console.error(
+err
+);
+
+return{
+
+success:false,
+
+message:
+err.message
+
+};
+
+}
 
 }
